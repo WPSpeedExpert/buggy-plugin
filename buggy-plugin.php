4d1e208be5e8bddcfd2743c870869e03afec5fc4
WP Speed Expert <brian@wpspeedexpert.com>
2024-12-29 17:49:17 +0100

    Improved by removing echo placeholder text

--- buggy-plugin.php
+++ buggy-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Buggy Plugin
  * Plugin URI:        https://octahexa.com
  * Description:       A deliberately buggy plugin for assessment purposes.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Octahexa
  * Author URI:        https://octahexa.com
  * License:           GPL2
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 add_action('init', 'buggy_function');
 
 function buggy_function() {
-    echo 'This will trigger a critical error after activation.';
+    echo '';
 
     // Call to an undefined function, causing a runtime fatal error.
     undefined_function_call(); // This function does not exist and will trigger a critical error.
